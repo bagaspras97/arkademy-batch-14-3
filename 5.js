5976f555b7bacbb2c982ebf75b4d435f53bc8efd
bagaspras97 <59312912+bagaspras97@users.noreply.github.com>
2020-01-11 04:24:45 -1000

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,10 @@
+
+var random = "randomize(5)"
+random = [1,3,5,3,9]
+console.log("arrray :" +"["+random+"]")
+var x=2;
+
+for (i=0; i<random.length; i++){
+  x +=random[i] 
+}
+console.log("sum :" + x)
