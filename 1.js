bdbde590b3708b6521643064c3b67556c5c5a81f
bagaspras97 <59312912+bagaspras97@users.noreply.github.com>
2020-01-11 04:23:14 -1000

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,45 @@
+var biodata =
+{
+"name" : "Bagas Prasetyadi",
+"age"  : 22,
+"address" : "Bantar Peuteuy Rt01/04 No.8 Tajur, Bogor Timur",
+"hobbies" : ["Running", "Badminton", "Swimming", "Playing Guitar"],
+"is_married" : false,
+"list_school":
+[
+  {
+   "name"    : "SMK Informatika Pesat",
+   "year_in" : "2012",
+   "year_out": "2015",
+   "major"   : "TKJ"
+  },
+  {
+   "name"    : "Universitas Gunadarma",
+   "year_in" : "2015",
+   "year_out": "2019",
+   "major"   : "Sistem Komputer"
+  }
+],  
+"skills" : 
+[
+  {
+   "name" : "Computer Networking",
+   "level": "beginner" 
+  },
+  {
+   "name" : "Database", 
+   "level": "beginner" 
+  },
+  {
+   "name" : "Web Programming",
+   "level": "beginner" 
+  }
+  
+],
+
+"interest_in_coding" : true,
+  
+};
+
+console.log(biodata)
+
